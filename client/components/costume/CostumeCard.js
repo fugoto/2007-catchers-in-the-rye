eae6ac6ef1203d23779bd80e2d05f4739620f270
Fu Goto <fu.goto@gmail.com>
2021-01-23 15:56:03 -0500

    styling of costume page

--- client/components/costume/CostumeCard.js
+++ client/components/costume/CostumeCard.js
@@ -1,62 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-class Costume extends Component {
-    render() {
-        const { 
-            costume, view, user 
-        } = this.props
-        return (
-            <div className={view === "detailed" ? "costume detailView" : "costume"} role="listitem">
+function Costume (props) {
+    const { 
+        costume, view, user 
+    } = props
+    return (
+        <div className={view === "detailed" ? "costume detailView" : "costume"} role="listitem">
+            <div>
+                <img src={costume.imageUrl} />
+            </div>
+            <div className="details">
+            <div className="costume-name">
+            {
+                view !== "detailed" ?
+                    <Link to={`/costumes/${costume.costumeName}/${costume.id}`}>{costume.costumeName}</Link>
+                    :
+                    costume.costumeName
+            }
+            </div>
                 <div>
-                    <img src={costume.imageUrl} />
+                    <span className="old-price">${(costume.price / (1 - costume.discount)).toFixed(2)}</span>
+                    <span className="new-price">  ${costume.price}</span>
                 </div>
-                <div className="details">
-                    <div className="costume-name">{costume.costumeName}</div> 
-                    <br />
-                    <div className="old">WAS</div>
-                    <div className="old-price">${(costume.price / (1 - costume.discount)).toFixed(2)}</div>
-                    <br />
-                    <div className="new">NOW ONLY</div>
-                    <div className="new-price">${costume.price}</div>
-                    <br />
-                    {
-                        view === "detailed" ?
+                {
+                    view === "detailed" ?
+                        (<div>
+                            <div>Material: {costume.material}</div>
+                            <div>Made In {costume.madeIn}</div>
+                            <div>One size fits most adults</div>
+                        </div>)
+                        :
+                        null
+                }  
+                <div className="links">
+                    { 
+                        user.userType === 'admin'?
                             (<div>
-                                <div>Material: {costume.material}</div>
-                                <div>Made In {costume.madeIn}</div>
-                                <div>One size fits most adults</div>
-                            </div>)
-                            :
-                                null
-                    }  
-                    <div className="links">
-                        {
-                            view !== "detailed" ?
-                                (<div>
-                                    <br />
-                                    <div><Link to={`/costumes/${costume.costumeName}/${costume.id}`}>Details</Link></div>
-                                </div>)
-                            :
-                                null
-                        } 
-                        { 
-                            user.userType === 'admin'?
-                                (<div>
-                                    <br />
-                                    <Link to={`/costumes/${costume.costumeName}/${costume.id}/${costume.categoryId}/admin`}>
-                                        Update
-                                    </Link>
-                                </div>) 
-                            : 
-                                null 
-                        }
-                    </div>
+                                <br />
+                                <Link to={`/costumes/${costume.costumeName}/${costume.id}/${costume.categoryId}/admin`}>
+                                    Update
+                                </Link>
+                            </div>) 
+                        : 
+                        null 
+                    }
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 const mapStateToProps = (state, ownProps) => ({
